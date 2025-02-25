89d1b427dbff0ddd13ea22ee4b32f3cf91b1aaa2
Edward <a50541853@gmail.com>
2025-02-25 15:25:22 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ async fn main() {
     });
 
     //获取路由
-    // // 定义一个简单的 GET 路由test
+    // // 定义一个简单的 GET 路由t
     // let hello = warp::path!("hello" / String)
     //     .map(|name| format!("Hello, {}!", name));
 
